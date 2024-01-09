058622cc7b8e54f3fa35d9aff1ec3b3e6fa3891f
Noie Glenn Manoy <97178346+Serevox@users.noreply.github.com>
2024-01-09 14:58:44 +0800

    Add files via upload

---
+++ LAB ACTIVITY 1 - NGM_WD401/Hello.jsx
@@ -0,0 +1,10 @@
+// Noie Glenn Manoy - WD 401
+// Blockchain
+
+export default function Hello() {
+    return( 
+        <div>
+        <Hello />
+        </div>
+    );
+}
---
+++ LAB ACTIVITY 1 - NGM_WD401/Welcome.jsx
@@ -0,0 +1,16 @@
+// Noie Glenn Manoy - WD 401
+// Blockchain
+
+import React from "react";
+import { render } from "react-dom";
+
+export default class Welcome extends React.Component{
+    render() {
+        return( 
+            <div>
+            <p>WELCOME NGM-2196</p>
+            </div>
+            );
+    }
+    
+}
