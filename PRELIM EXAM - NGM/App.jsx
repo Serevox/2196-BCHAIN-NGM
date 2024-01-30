6d0e65f21994733da1b08c865acdd6e3957949b7
Noie Glenn Manoy <97178346+Serevox@users.noreply.github.com>
2024-01-30 14:53:03 +0800

    Add files via upload

---
+++ PRELIM EXAM - NGM/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Prelim from './components/prelimexam'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Prelim />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ PRELIM EXAM - NGM/Prelimexam.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import './prelim.css'
+
+function Tama(){
+    const styles = { 
+        div: {
+            backgroundColor: "#008000"
+        },
+        p: {
+            backgroundColor: "#FF0000"
+        }
+    }
+    return (
+        <div>
+             <h1>Things to Bring</h1>
+             <div className="green">
+                <h3>My strawhat</h3>
+                <h3>Gum gum Fruit</h3>
+                <h3 className="red">The Red Vest</h3>
+             </div>
+        </div>
+       
+    );
+}
+
+function Mali(){
+    return (
+        <div>
+             <h1>Things to Bring</h1>
+             <div>
+                <h3>My strawhat</h3>
+                <h3>Gum gum Fruit</h3>
+                <h3>The Red Vest</h3>
+             </div>
+        </div>
+    )
+}
+
+function Check(props){
+    const isCheck = props.isCheck;
+    return isCheck ? <Tama /> : <Mali />
+}
+
+export default function Prelim(){
+    return(
+        <div>
+            <Check isCheck={true}/>
+        </div>
+    )
+}
